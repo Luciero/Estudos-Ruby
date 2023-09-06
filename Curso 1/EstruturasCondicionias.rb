29f2bf7e668e422ceb329ff773e78dbd87adc39e
Luciero <pedrohs@ufba.br>
2023-09-06 19:23:25 -0300

    Estruturas Condicionais

---
+++ Curso 1/EstruturasCondicionias.rb
@@ -0,0 +1,13 @@
+# Analisando um dia da semana
+dia = "terca"
+if dia == "domingo"
+    almoco = "especial"
+
+elsif dia == "feriado"
+    almoco = "mais tarde"
+    
+else
+    almoco = "normal"
+end
+puts "Hoje o almoco vai ser #{almoco}"
+
